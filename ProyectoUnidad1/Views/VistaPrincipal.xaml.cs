492ed905ca0ab917e91654817280062733e5ed06
AredJ-ui <abisaicantu5@gmail.com>
2026-09-10 21:16:31 -0600

    Vista principal added

---
+++ ProyectoUnidad1/Views/VistaPrincipal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoUnidad1.Views
+{
+    /// <summary>
+    /// Interaction logic for VistaPrincipal.xaml
+    /// </summary>
+    public partial class VistaPrincipal : Window
+    {
+        public VistaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
